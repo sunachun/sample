4c41790265c3f137444c9664732e4053f404239c
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-10-28 16:27:55 +0900

    画像アップロード

--- lib/add_book/add_book_page.dart
+++ lib/add_book/add_book_page.dart
@@ -13,45 +13,74 @@ class AddBookPage extends StatelessWidget {
         ),
         body: Center(
           child: Consumer<AddBookModel>(builder: (context, model, child) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: '本のタイトル',
+            return Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      GestureDetector(
+                        child: SizedBox(
+                          width: 100,
+                          height: 160,
+                          child: model.imageFile != null
+                              ? Image.file(model.imageFile)
+                              : Container(
+                                  color: Colors.grey,
+                                ),
+                        ),
+                        onTap: () async {
+                          await model.pickImage();
+                        },
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: '本のタイトル',
+                        ),
+                        onChanged: (text) {
+                          model.title = text;
+                        },
+                      ),
+                      SizedBox(height: 8),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: '本の著者',
+                        ),
+                        onChanged: (text) {
+                          model.author = text;
+                        },
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () async {
+                          try {
+                            model.startLoading();
+                            await model.addBook();
+                            Navigator.of(context).pop(true);
+                          } catch (e) {
+                            final snackBar = SnackBar(
+                              backgroundColor: Colors.red,
+                              content: Text(e.toString()),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          } finally {
+                            model.endLoading();
+                          }
+                        },
+                        child: Text('追加する'),
+                      ),
+                    ],
+                  ),
+                ),
+                if (model.isLoading)
+                  Container(
+                    color: Colors.black54,
+                    child: Center(
+                      child: CircularProgressIndicator(),
                     ),
-                    onChanged: (text) {
-                      model.title = text;
-                    },
                   ),
-                  SizedBox(height: 8),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: '本の著者',
-                    ),
-                    onChanged: (text) {
-                      model.author = text;
-                    },
-                  ),
-                  SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () async {
-                      try {
-                        await model.addBook();
-                        Navigator.of(context).pop(true);
-                      } catch (e) {
-                        final snackBar = SnackBar(
-                          backgroundColor: Colors.red,
-                          content: Text(e.toString()),
-                        );
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      }
-                    },
-                    child: Text('追加する'),
-                  ),
-                ],
-              ),
+              ],
             );
           }),
         ),
